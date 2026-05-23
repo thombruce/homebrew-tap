462290372932e83eac2339b673e13ca1bc8b8438
Thom Bruce <thom@thombruce.com>
2026-05-23 13:04:36 +0100

    feat: update flexi formula to v0.2.0

--- Formula/flexi.rb
+++ Formula/flexi.rb
@@ -1,28 +1,28 @@
 class Flexi < Formula
   desc "A minimal CLI tool for tracking your flexi-time balance"
   homepage "https://github.com/thombruce/flexi"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/thombruce/flexi/releases/download/v#{version}/flexi-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "20558a31f664f425970edcb9e139eb798590d419b5ef510c73696f10cdfc0c30"
+      sha256 "67bbeb102ccbc827cc64593ad1be7b16fa26fc5c89ec3ffdb5b22a0c1958faec"
     end
     on_intel do
       url "https://github.com/thombruce/flexi/releases/download/v#{version}/flexi-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "4c931fc5d6ada4fa83520223971abda1bbbc00985bfd9bedfecb0e744be0281c"
+      sha256 "e40ee819acfad5e0a8c2a88e8cf5c233580ef620f7c881cffc15491079665c8b"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/thombruce/flexi/releases/download/v#{version}/flexi-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "4adbf7e2497d185ded02ba25d07f76fcfcfbbe6ec2b700aaf17fc943d184ebf7"
+      sha256 "9129f834543420e232916e9ef702ca6c6bea177138abed6f2f572ed1b4efe7c7"
     end
     on_intel do
       url "https://github.com/thombruce/flexi/releases/download/v#{version}/flexi-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "13ec992ad780ec007a6d249cd98a0bedbc8f14be18ca7901e2fbbbe95c49eca8"
+      sha256 "520288d13bac237dde756db782f6b1559bcc729f2db08439ee047f93152686fe"
     end
   end
 
